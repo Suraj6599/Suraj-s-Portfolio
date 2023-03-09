5f656135693acff98c08588f5aeea24f456fc0fb
Suraj6599 <116073622+Suraj6599@users.noreply.github.com>
2023-03-09 13:01:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const BLACKLISTED_KEY_CODES = [ 38 ];
+const COMMANDS = {
+  help:
+    'Supported commands: <span class="code">about</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">certifications</span>, <span class="code">contact</span>',
+  about:
+  "Experience in manual penetration and application testing. Profound knowledge of operating systems, application software and cyber security tools. Huge knowledge of managing information assurance evaluation tests. Wide knowledge of scripting language Python. Proficiency in Linux operating systems and windows. Ability to exploit recognized vulnerabilities.",
+  skills:
+    "Ethical Hacking | Penetration Testing|",
+  education:
+    "Bachelor of Technology In Computer Science And Engineering",
+  certification: 
+    "Certified App Practioner",
+  contact:
+    "You can contact me on Linkedin:<br><a href='https://www.linkedin.com/in/suraj-choudhari-5b5267218/' class='success link'>Linkedin</a>"
+};
+let userInput, terminalOutput;
+
+const app = () => {
+  userInput = document.getElementById("userInput");
+  terminalOutput = document.getElementById("terminalOutput");
+  document.getElementById("dummyKeyboard").focus();
+  console.log("Application loaded");
+};
+
+const execute = function executeCommand(input) {
+  let output;
+  input = input.toLowerCase();
+  if (input.length === 0) {
+    return;
+  }
+  output = `<div class="terminal-line"><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+  if (!COMMANDS.hasOwnProperty(input)) {
+    output += `<div class="terminal-line">no such command: ${input}</div>`;
+    console.log("Oops! no such command");
+  } else {
+    output += COMMANDS[input];
+  }
+
+  terminalOutput.innerHTML = `${
+    terminalOutput.innerHTML
+  }<div class="terminal-line">${output}</div>`;
+  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+};
+
+const key = function keyEvent(e) {
+  const input = userInput.innerHTML;
+
+  if (BLACKLISTED_KEY_CODES.includes(e.keyCode)) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    execute(input);
+    userInput.innerHTML = "";
+    return;
+  }
+
+  userInput.innerHTML = input + e.key;
+};
+
+const backspace = function backSpaceKeyEvent(e) {
+  if (e.keyCode !== 8 && e.keyCode !== 46) {
+    return;
+  }
+  userInput.innerHTML = userInput.innerHTML.slice(
+    0,
+    userInput.innerHTML.length - 1
+  );
+};
+
+document.addEventListener("keydown", backspace);
+document.addEventListener("keypress", key);
+document.addEventListener("DOMContentLoaded", app);
